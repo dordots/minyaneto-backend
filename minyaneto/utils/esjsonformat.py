45f83f55c5c024e42ae38dae4630f23e56d67247
shoamtal <shoamtal@gmail.com>
2018-02-23 10:53:44 +0200

    return geo points as float

--- minyaneto/utils/esjsonformat.py
+++ minyaneto/utils/esjsonformat.py
@@ -2,7 +2,7 @@ def synagogue_format(synagogue):
     synagogue_src = synagogue["_source"]
     return {
         "id": synagogue["_id"],
-        "geo": synagogue_src['geo'],
+        "geo": {'lat': float(synagogue_src['geo']['lat']), 'lon': float(synagogue_src['geo']['lon'])},
         "name": synagogue_src['name'],
         "address": synagogue_src.get("address"),
         "classes": synagogue_src.get('classes'),
